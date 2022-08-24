df9551e3c06b53fa235c0b2d2b39dde1b5937629
AkshayaJeyan <93139984+AkshayaJeyan@users.noreply.github.com>
2022-08-24 06:36:36 -0400

    Update TestTrigger.bat

--- TestTrigger.bat
+++ TestTrigger.bat
@@ -1,2 +1,3 @@
-"C:\Users\rpa_uat\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "C:\actions-runner\_work\UiPath-Test\UiPath-Test\Main.xaml"
 tscon 2 /dest:console
+"C:\Users\rpa_uat\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "C:\actions-runner\_work\UiPath-Test\UiPath-Test\Main.xaml"
+
