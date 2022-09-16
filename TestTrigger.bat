4aee6dba01f4d83a417899953a07beed50aa030b
AkshayaJeyan <93139984+AkshayaJeyan@users.noreply.github.com>
2022-09-16 15:08:54 -0400

    Update TestTrigger.bat

--- TestTrigger.bat
+++ TestTrigger.bat
@@ -1,1 +1,1 @@
-"C:\actions-runner\_work\UiPath\Studio\UiRobot.exe" -file "C:\actions-runner\_work\UiPath-Test\UiPath-Test\Main.xaml"
+"C:\Users\rpa_uat\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "C:\actions-runner\_work\UiPath-Test\UiPath-Test\Main.xaml"
